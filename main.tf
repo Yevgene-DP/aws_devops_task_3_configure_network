41cf6b9d60a1b7d2e57ccf83e14562de4c7b3fb3
Yevgene-DP <ebalalay@gmail.com>
2025-11-09 18:35:52 +0200

    Complete AWS DevOps Task 3: Network configuration

--- main.tf
+++ main.tf
@@ -1,13 +1,110 @@
-# Add your code here: 
+# Отримати публічну IP-адресу
+data "http" "my_public_ip" {
+  url = "https://checkip.amazonaws.com"
+}
 
-# 1. Create a subnet 
-# 2. Create an Internet Gateway and attach it to the vpc
-# 3. Configure routing for the Internet Gateway
-# 4. Create a Security Group and inbound rules 
-# 5. Uncommend (and update the value of security_group_id if required) outbound rule - it required 
-# to allow outbound traffic from your virtual machine: 
-# resource "aws_vpc_security_group_egress_rule" "allow_all_eggress" {
-#   security_group_id = aws_security_group.security_group.id
-#   cidr_ipv4   = "0.0.0.0/0"
-#   ip_protocol = -1
-# }
+# Створити VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Створити Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Створити Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Створити Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Асоціювати Subnet з Route Table
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Створити Security Group
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+
+# Дозволити HTTP трафік з усіх IP
+resource "aws_security_group_rule" "http_ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow HTTP from anywhere"
+}
+
+# Дозволити HTTPS трафік з усіх IP  
+resource "aws_security_group_rule" "https_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow HTTPS from anywhere"
+}
+
+# Дозволити SSH тільки з вашої IP-адреси
+resource "aws_security_group_rule" "ssh_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["${chomp(data.http.my_public_ip.response_body)}/32"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow SSH from my IP"
+}
+
+# Дозволити весь вихідний трафік
+resource "aws_security_group_rule" "all_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow all outbound traffic"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "subnet_id" {
-    description = "ID of the VPC subnet, deployed by the module"
-    value       = ""
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
 output "security_group_id" {
-    description = "ID of the security group, deployed by the module"
-    value       = ""
+  description = "ID of the security group"
+  value       = aws_security_group.web_sg.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet" 
+  value       = aws_subnet.public.id
 }
